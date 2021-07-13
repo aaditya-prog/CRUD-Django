fd096e2ef854df884b857f30e4bde6191c2bdbff
Aaditya Dulal <np01cp4a190231@islingtoncollege.edu.np>
2021-07-13 13:11:53 +0545

    Migrating to postgres

--- userCRUD/settings.py
+++ userCRUD/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = "userCRUD.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
@@ -97,13 +101,13 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        "NAME": ("django.contrib.auth.password_validation.MinimumLengthValidator"),
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": ("django.contrib.auth.password_validation.CommonPasswordValidator"),
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        "NAME": ("django.contrib.auth.password_validation.NumericPasswordValidator"),
+        'NAME': "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
