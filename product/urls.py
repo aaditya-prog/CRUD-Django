9e96f801241be2331d020fbb0f2a544022321b76
Aaditya Dulal <np01cp4a190231@islingtoncollege.edu.np>
2021-07-14 10:20:07 +0545

    Fixed view products

--- product/urls.py
+++ product/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = "product"
 urlpatterns = [
     path("", views.productAdd, name="productadd"),
-    path("productread", views.ProductView, name="productread"),
+    path("productread/", views.ProductView, name="productread"),
     path("deleteproduct/<int:id>", views.delete_product, name="deleteproduct"),
     path("<int:id>", views.update_product, name="updateproduct"),
 ]
